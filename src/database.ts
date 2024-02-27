24306e64b13b8124a6f5bb1d8dc441e9e2120197
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-27 20:44:59 -0300

    feat: environment vars

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,13 @@
 import { knex as setupKnex, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env is not found.')
+}
+
 export const knexConfig: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
